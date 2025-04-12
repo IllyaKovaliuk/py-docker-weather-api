e772b03a231573f3d8b2631cd6d11b056a059fa7
Illya Kovaliuk <kovalukilla271@gmail.com>
2025-04-13 01:39:43 +0300

    Solution 2

--- app/main.py
+++ app/main.py
@@ -7,21 +7,23 @@ load_dotenv()
 base_url = "http://api.weatherapi.com/v1"
 
 
-def get_weather(city) -> None:
+def get_weather(city: str) -> None:
     api_key = os.getenv("api_key")
     url = f"{base_url}/current.json?key={api_key}&q={city}"
     response = requests.get(url)
 
     if response.status_code == 200:
         weather_info = response.json()
-        city_name = weather_info['location']['name']
-        country = weather_info['location']['country']
-        localtime = weather_info['location']['localtime']
-        temp_c = weather_info['current']['temp_c']
-        print(f"City: {city_name}, Country: {country}, Local Time: {localtime}, Temperature: {temp_c}°C")
+        city_name = weather_info["location"]["name"]
+        country = weather_info["location"]["country"]
+        localtime = weather_info["location"]["localtime"]
+        temp_c = weather_info["current"]["temp_c"]
+        print(f"City: {city_name}, "
+              f"Country: {country}, "
+              f"Local Time: {localtime}, "
+              f"Temperature: {temp_c}°C")
     else:
         return print("Failed to find data")
-
 
 
 if __name__ == "__main__":
